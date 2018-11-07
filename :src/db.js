0d270789e6dfaff7296c1c3eaeb2a6e16ece44c4
mikeanastasio <mikeanastasio42@gmail.com>
2018-11-07 14:15:08 -0500

    Added two mongoose schema as well as skeleton readme.md

--- :src/db.js
+++ :src/db.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose');
 
-/*Schema
-const soundSchema = new mongoose.Schema({
-	what: String,
-    where: String,
-    date: String,
-    hour: Number,
-    desc: String
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: String,
+    favoriteColor: String,
+    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
 });
-mongoose.model("Sound", soundSchema);
-*/
+
+const postSchema = new mongoose.Schema({
+    photoUrl: String,
+    description: String,
+    timePosted: String,
+    longitude: String,
+    latitude: String
+});
+mongoose.model("User", userSchema);
+mongoose.model("Post", postSchema);
 
 //connect
 mongoose.connect('mongodb://localhost/final', { useNewUrlParser: true });
